fdea716e5cc45864982ffe89d8c3694b4d54c026
reneetoscan <renee.toscan@outlook.com>
2023-04-21 12:38:29 -0700

    reworked into color class and testing backgroundc

--- colorpicker.js
+++ colorpicker.js
@@ -9,19 +9,35 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  class Color {
+    constructor () {
+      this.innerContainer = document.createElement('div');
+      this.outerContainer = document.createElement('div');
+    }
+  }
   //on button click, call EyeDropper API and display color in pickedColor div
   document.querySelector('button').addEventListener('click', () => {
-    const pickedColor = document.querySelector('#result');
+    //create new instance of Color class
+    const pickedColor = new Color;
+    //assign class names to inner and outer containers
+    pickedColor.innerContainer.className = 'inner-container';
+    pickedColor.outerContainer.className = 'outer-container';
+    //get results-container div and store under resultsContainer label
+    const resultsContainer = document.getElementById('results-container');
+    //append to document
+    pickedColor.outerContainer.appendChild(pickedColor.innerContainer);
+    resultsContainer.appendChild(pickedColor.outerContainer);
+    document.body.appendChild(resultsContainer);
+
     const eyeDropper = new EyeDropper();
     eyeDropper
       .open()
       .then((result) => {
-        pickedColor.innerHTML = result.sRGBHex;
-        pickedColor.style.backgroundColor = result.sRGBHex;
+        pickedColor.outerContainer.innerHTML = result.sRGBHex;
+        pickedColor.innerContainer.style.backgroundColor = result.sRGBHex;
       })
       .catch((e) => {
-        pickedColor.innerHTML = e;
+        alert(e);
       })
   })
 })
