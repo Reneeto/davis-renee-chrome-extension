4cb54f3a0b322b976842233a47754e4e0c3024a6
Davis Kim <dkim0509@gmail.com>
2023-04-20 22:35:19 -0400

    not much

--- colorpicker.js
+++ colorpicker.js
@@ -5,4 +5,18 @@
     //user will hover certain elements
         //stretch - magnifying glass that magnifies the color 
     //when user clicks on an element, return the RGB color of whatever the user clicked on
-    //display the RGB color within the actual extension 
+    //display the RGB color within the actual extension
+
+//run function when extension pop-up buttosn is clicked
+// document.addEventListener('DOMContentLoaded', () => {
+
+function findColor () {
+    console.log('hello world');
+}
+
+// const button = document.querySelector("#button");
+// button.addEventListener("click", findColor);
+
+//function for when user clicks on an element 
+// })
+console.log('hello world');
