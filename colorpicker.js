691685128ef5b4b64ec7eda9bdf5459ce6753651
reneetoscan <renee.toscan@outlook.com>
2023-04-20 21:47:30 -0700

    pseudocoded api call so not just copy-pasting code

--- colorpicker.js
+++ colorpicker.js
@@ -1,22 +1,22 @@
-//user functionality 
-//click on the icon for the extension __X__
-//shows a popup __X__
-//there's a button that says "pick color" -> invoke the function (?) utilizing eventlistener
-//user will hover certain elements
-//stretch - magnifying glass that magnifies the color 
-//when user clicks on an element, return the RGB color of whatever the user clicked on
-//display the RGB color within the actual extension
+// //user functionality 
+// //click on the icon for the extension __X__
+// //shows a popup __X__
+// //there's a button that says "pick color" -> invoke the function (?) utilizing eventlistener
+// //user will hover certain elements
+// //stretch - magnifying glass that magnifies the color 
+// //when user clicks on an element, return the RGB color of whatever the user clicked on
+// //display the RGB color within the actual extension
 
-//run function when extension pop-up buttosn is clicked
+// //run function when extension pop-up buttosn is clicked
 document.addEventListener('DOMContentLoaded', () => {
-
-function findColor() {
-  console.log('hello world');
-}
-
-const button = document.querySelector("#button");
-button.addEventListener("click", findColor);
-
-//function for when user clicks on an element 
+  //on button click
+    //store ID result div in resultElement constant
+    //create new EyeDropper instance and store in eyeDropper label
+    //for eyeDropper
+      //use open method 
+      //then 
+        //add result text to resultElement
+        //make background color of resultElement the result color
+      //catch 
+        //resultElement text content something
 })
-// console.log('hello world');
