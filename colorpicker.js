75d2ce50af1c99927517a95ec9c277554ead7aec
reneetoscan <renee.toscan@outlook.com>
2023-04-20 19:53:24 -0700

    console logs hello world!?!?!

--- colorpicker.js
+++ colorpicker.js
@@ -1,22 +1,22 @@
 //user functionality 
-    //click on the icon for the extension __X__
-    //shows a popup __X__
-    //there's a button that says "pick color" -> invoke the function (?) utilizing eventlistener
-    //user will hover certain elements
-        //stretch - magnifying glass that magnifies the color 
-    //when user clicks on an element, return the RGB color of whatever the user clicked on
-    //display the RGB color within the actual extension
+//click on the icon for the extension __X__
+//shows a popup __X__
+//there's a button that says "pick color" -> invoke the function (?) utilizing eventlistener
+//user will hover certain elements
+//stretch - magnifying glass that magnifies the color 
+//when user clicks on an element, return the RGB color of whatever the user clicked on
+//display the RGB color within the actual extension
 
 //run function when extension pop-up buttosn is clicked
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-function findColor () {
-    console.log('hello world');
+function findColor() {
+  console.log('hello world');
 }
 
-// const button = document.querySelector("#button");
-// button.addEventListener("click", findColor);
+const button = document.querySelector("#button");
+button.addEventListener("click", findColor);
 
 //function for when user clicks on an element 
-// })
-console.log('hello world');
+})
+// console.log('hello world');
