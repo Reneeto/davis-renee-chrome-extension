25b591e527d79e6ca998d7247df4db889b4fd039
Davis Kim <dkim0509@gmail.com>
2023-04-22 10:27:57 -0400

    refresh button

--- colorpicker.js
+++ colorpicker.js
@@ -19,11 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const innerContainer = document.createElement('div');
         const outerContainer = document.createElement('div');
         const outerContainerText = document.createElement('div');
+
         outerContainerText.innerHTML = result.sRGBHex;
         innerContainer.style.backgroundColor = result.sRGBHex;
+
         innerContainer.className = "inner-container";
         outerContainer.className = "outer-container";
         outerContainerText.className = "hex-code";
+
         resultsContainer.appendChild(outerContainer);
         outerContainer.appendChild(innerContainer);
         outerContainer.appendChild(outerContainerText);
@@ -31,12 +34,24 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((e) => {
         alert(e);
       })
-
-
-
-
-
     //append to document
-
+  
   })
+});
+
+  document.querySelector('#refresh').addEventListener('click', () => {
+    function removeElementsByClass(className){
+      const elements = document.getElementsByClassName(className);
+      while(elements.length > 0){
+          elements[0].parentNode.removeChild(elements[0]);
+      }
+    }
+    removeElementsByClass("outer-container");
+    
+    // const outerDiv = document.querySelector(".outer-container");
+    // const innerDiv = document.querySelector(".inner-container");
+    // const hexDiv = document.querySelector(".hex-code");
+    // outerDiv.classList.remove("outer-container");
+    // innerDiv.classList.remove("inner-container");
+    // hexDiv.classList.remove("hex-code");
 })
