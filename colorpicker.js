99328d783bf470573b2dd0f41b526639ca04b730
Davis Kim <dkim0509@gmail.com>
2023-04-22 12:51:09 -0400

    conditional if there is no colors

--- colorpicker.js
+++ colorpicker.js
@@ -57,13 +57,14 @@ document.querySelector('#refresh').addEventListener('click', () => {
 })
 
 function exportHTML(){
+  const outerContainer = document.querySelector('.outer-container');
+  if (!outerContainer) return alert("You have no colors to export!")
   var header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
        "xmlns:w='urn:schemas-microsoft-com:office:word' "+
        "xmlns='http://www.w3.org/TR/REC-html40'>"+
        "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
   var footer = "</body></html>";
   var sourceHTML = header+document.getElementById("results-container").innerHTML+footer;
-  
   var source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
   var fileDownload = document.createElement("a");
   document.body.appendChild(fileDownload);
